dd77d649afbed342f41fda02b8960b1ff357db3f
riza55358@gmail.com <106018548+Dwkyyrz@users.noreply.github.com>
2024-09-19 22:26:42 +0700

    update view

---
+++ resources/views/header.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- set the encoding of your site -->
+    <title>Ruang | Rotonengen</title>
+    <!-- favicons Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png" />
+    <link rel="shortcut icon" type="image/png" sizes="48x48" href="images/favicons/favicon.ico" />
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="images/favicons/site.webmanifest" />
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<!-- set the page title -->
+	<title>Scavvios | One Page Parallax HTML5 Template</title>
+	<!-- inlcude google poppins font cdn link -->
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+	<!-- include the site bootstrap stylesheet -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- include the site stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- include theme color setting stylesheet -->
+	<link rel="stylesheet" href="css/colors.css">
+	<!-- include the site responsive stylesheet -->
+	<link rel="stylesheet" href="css/responsive.css">
+	
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<!-- set the encoding of your site -->
-    <title>Ruang | Rotonengen</title>
-    <!-- favicons Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png" />
-    <link rel="shortcut icon" type="image/png" sizes="48x48" href="images/favicons/favicon.ico" />
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicons/favicon-16x16.png" />
-    <link rel="manifest" href="images/favicons/site.webmanifest" />
+@include('header')
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<!-- set the page title -->
-	<title>Scavvios | One Page Parallax HTML5 Template</title>
-	<!-- inlcude google poppins font cdn link -->
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
-	<!-- include the site bootstrap stylesheet -->
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<!-- include the site stylesheet -->
-	<link rel="stylesheet" href="css/style.css">
-	<!-- include theme color setting stylesheet -->
-	<link rel="stylesheet" href="css/colors.css">
-	<!-- include the site responsive stylesheet -->
-	<link rel="stylesheet" href="css/responsive.css">
-	
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
 	<!-- pageWrapper -->
 	<div id="pageWrapper">
 		<!-- phStickyWrap -->
@@ -39,7 +10,7 @@
 					<div class="navbar-container">
 							<div class="logo">
 									<a href="/">
-										<img src="images/favicons/ruangroto.png" class="img-fluid" alt="scavvios">
+										<img src="images/favicons/ruangroto.png" class="img-fluid" alt="Ruangroto">
 									</a>
 							</div>
 							<input type="checkbox" id="menu-toggle" class="menu-toggle">
@@ -302,7 +273,8 @@
 						
 				</main>
 				</div>
-				<script>
+
+<script>
     $(document).ready(function() {
         $('#contactForm').on('submit', function(e) {
             e.preventDefault(); // Mencegah form untuk submit secara default
@@ -330,15 +302,4 @@
     });
 </script>
 
-	<!-- include jQuery library -->
-	<script src="js/jquery-3.4.1.min.js"></script>
-	<!-- include bootstrap popper JavaScript -->
-	<script src="js/popper.min.js"></script>
-	<!-- include bootstrap JavaScript -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- include custom JavaScript -->
-	<script src="js/jqueryCustom.js"></script>
-	<!-- include fontAwesome -->
-	<script src="https://kit.fontawesome.com/391f644c42.js"></script>
-</body>
-</html>
+@include('footer')
